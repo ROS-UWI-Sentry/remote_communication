c70c2a9999d68c9628c7fb73ebfb70fae3b0fc52
Ian Sooknanan <ian.sooknanan@my.uwi.edu>
2021-11-22 17:04:16 -0400

    small changes, frombrowser to remote

--- scripts/browser_reports.py
+++ scripts/browser_reports.py
@@ -36,7 +36,7 @@
                         ###note###
 # ____________________________________________________
 #|This is a ROS node that listens to a message from   |
-#|the browser.                                        |
+#|the remote.                                         |
 #|It is for creating and saving report files and      |
 #|sending the data in one string variable.            |
 #|See accompanying flowchart for overall operation    |
@@ -52,7 +52,7 @@ from std_msgs.msg import String
 from std_msgs.msg import Int32
 from multiprocessing import Process, Pipe
 import thread
-#variable to tell when to start sending the data from the file to the browser
+#variable to tell when to start sending the data from the file to the remote
 #it is unused
 startSending = 0
 
@@ -73,9 +73,9 @@ def callback(data):
     elif (data.data=="Send Report"):
         #startSending=1
         #open file and read the file into temp variable
-        with open("/home/uwi/catkin_ws/src/browser_communication/scripts/reports.txt",'r') as f:
+        with open("/home/uwi/catkin_ws/src/remote_communication/scripts/reports.txt",'r') as f:
             temp=f.read().splitlines()
-        #publish the variable to the browser
+        #publish the variable to the remote
         pub.publish(str(temp))
 
 
@@ -84,12 +84,12 @@ def callback(data):
         startSending=0
     elif (data.data=="Clear Report"):
         #open the file such that it clears everything then close it
-        f=open("/home/uwi/catkin_ws/src/browser_communication/scripts/reports.txt","w")
+        f=open("/home/uwi/catkin_ws/src/remote_communication/scripts/reports.txt","w")
         f.close()
     else:
         #if the data is not recognized, this means that user data is coming in
         #open the file and write to it the incoming data
-        with open("/home/uwi/catkin_ws/src/browser_communication/scripts/reports.txt", 'a') as f:
+        with open("/home/uwi/catkin_ws/src/remote_communication/scripts/reports.txt", 'a') as f:
             f.write(data.data+"\n")
         rospy.loginfo(rospy.get_caller_id() + 'this is a test')
 
